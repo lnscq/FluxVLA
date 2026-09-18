8280e4ea3e97a9d317c1f36fcf23f23e502888b0
lnscq <91482597+lnscq@users.noreply.github.com>
2026-09-18 18:19:17 +0800

    feat(rl-config): add LIBERO and RoboTwin PPO recipes

---
+++ configs/rl/model/pi05_robotwin.py
@@ -0,0 +1,10 @@
+"""Same Flux PI0.5 parameter layout; reference OpenPI FP32 flow projections."""
+_base_ = '../../pi05/pi05_paligemma_libero_10_full_finetune.py'
+
+model = dict(
+    n_action_steps=50,
+    num_steps=5,
+    attention_implementation='eager',
+    openpi_fp32_flow=True,
+    vision_backbone=dict(openpi_stem_fp32=True),
+)
